b0c2e0f03b45a9a092059437140bd90e4d7a1bf3
el sedy ba <elsedy000@gmail.com>
2023-02-27 17:03:45 +0000

    rename methode

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from doc_plus_pertinent import doc_plus_pertinent
 app = Flask(__name__)
 
 @app.route('/api/v1/articles/<string:sujet_rechercher>', methods=['GET'])
-def process_json(sujet_rechercher):
+def get_document(sujet_rechercher):
 
     response = doc_plus_pertinent(sujet_rechercher)
 
